ea678fa903943a7404f4e0de6cd85f5883b34ab1
Dmitry <zuevmail@yandex.ru>
2021-08-25 17:02:50 +0300

    добавлено решение второго задания

---
+++ test.sql
@@ -0,0 +1,25 @@
+﻿
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+
+CREATE TABLE [dbo].[Category](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+
+CREATE TABLE [dbo].[ProductCategory](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProductId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+
+
+SELECT 
+	dbo.Products.Name as Product,
+	dbo.Category.Name as Category
+FROM dbo.Products
+
+LEFT JOIN dbo.ProductCategory ON dbo.Products.Id = dbo.ProductCategory.ProductId
+LEFT JOIN dbo.Category ON dbo.Category.Id = dbo.ProductCategory.CategoryId
